5d8e73da25acf19631afe3af98f7c7ea62206cb8
ammaar205 <119492653+ammaar205@users.noreply.github.com>
2023-03-14 16:36:08 +0000

    Add files via upload
    from 30DaysOfJavaScript file

---
+++ datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof 'food') // string
+console.log(typeof '9.81') // float
+console.log(typeof '70')  // integer
+console.log(typeof 'false') //boolean
---
+++ variable.js
@@ -0,0 +1,4 @@
+let burger = 'food'
+let gravity = '9.81'
+let weight ='70'
+let married = 'false'
